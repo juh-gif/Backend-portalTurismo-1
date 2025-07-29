a69a3e395e599e647f20161cf161581759ff2f41
ProfJeremiasONunes <13885@senacminas.edu.br>
2025-07-29 15:35:57 -0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const userController = require('../controllers/userController')
-const User = require('../models/Users');
+
 
 // ✅ Criar usuário (registro) — público
 router.post('/', userController.createUser);
