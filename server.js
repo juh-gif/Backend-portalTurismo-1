a83bd640c2091657a223978c26216c86d058dac7
juh-gif <1026006@senacminas.edu.br>
2025-08-01 15:06:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.get('/', (req, res)=> res.send('api funcionando'))
  
 app.use('/api/users', userRoutes)
-app.use('/api/login', authRoutes)
+app.use('/api/auth', authRoutes)
  app.use('/api/contact' , contactRoutes)
 const PORT = process.env.PORT;
  
