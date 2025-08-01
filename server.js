e25607582e88ffd577250e626c7d3a46c4139527
juh-gif <1026006@senacminas.edu.br>
2025-08-01 15:02:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
  
 app.use(cors({
-  origin: 'https://frontend-portal-turismo-nine.vercel.app/',
+  origin: 'https://frontend-portal-turismo-nine.vercel.app',
   methods: ['GET','POST','PUT','DELETE'],
   credentials: true
 }));
